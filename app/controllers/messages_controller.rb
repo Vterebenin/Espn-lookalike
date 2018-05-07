7c549abf27de376d0312d291411eb67aa5b7a7fd
Vterebenin <niceserver11@mail.ru>
2018-05-07 17:21:26 +0300

    Add functions for Comments

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -6,14 +6,15 @@ class MessagesController < ApplicationController
   end
 
   def show
+  	@user = current_user
   end
 
   def new
-  	@message = Message.new
+  	@message = current_user.messages.build
   end
 
   def create
-  	@message = Message.new(message_params)
+  	@message = current_user.messages.build(message_params)
   	if @message.save
   		redirect_to root_path
   	else
